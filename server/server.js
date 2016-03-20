e10a06e08e7871ed97e259e885c05f0905085787
Fanny Vasquez <fannyvr18@gmail.com>
2016-03-19 20:03:45 -0700

    (feat) receiving auth from google

--- server/server.js
+++ server/server.js
@@ -7,6 +7,34 @@ const session = require('koa-session');
 const passport = require('./controllers/auth');
 const routes = require('./routes/routes');
 const app = koa();
+const passport = require('koa-passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;;
+const tokens = require('./config');
+
+passport.use(new GoogleStrategy({
+    clientID: tokens.GOOGLE_CLIENT_ID,
+    clientSecret: tokens.GOOGLE_CLIENT_SECRET,
+    returnURL: 'http://localhost:3000/auth/google/return',
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function (accessToken, refreshToken, profile, done) {
+    console.log("USER PROFILE", accessToken); //profile contains all the personal data returned 
+    done(null, profile)
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.keys = ['secret'];
 app.use(session(app));
@@ -16,12 +44,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 routes(app);
 
-// Serve up react SPA
-app.use(spa(path.join(__dirname, '../dist'), {
-  index: 'index.html',
-  404: '404.html',
-  routeBase: '/',
-}));
+
 
 app.listen(3000);
 console.log('server running on port 3000');
