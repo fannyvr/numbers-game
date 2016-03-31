78f473c277cbf99e31bbc2a31ea3cb0c63d20186
Fanny Vasquez <fannyvr18@gmail.com>
2016-03-31 12:39:38 -0700

    (fix)rebase conflicts

--- server/server.js
+++ server/server.js
@@ -11,30 +11,8 @@ const passport = require('koa-passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;;
 const tokens = require('./config');
 
-passport.use(new GoogleStrategy({
-    clientID: tokens.GOOGLE_CLIENT_ID,
-    clientSecret: tokens.GOOGLE_CLIENT_SECRET,
-    returnURL: 'http://localhost:3000/auth/google/return',
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function (accessToken, refreshToken, profile, done) {
-    console.log("USER PROFILE", accessToken); //profile contains all the personal data returned 
-    done(null, profile)
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 
 app.keys = ['secret'];
 app.use(session(app));
